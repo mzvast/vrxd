de79e3d968629467f3a3ff194d910329a53829c6
mzvast <mzvast@gmail.com>
2020-08-14 16:59:08 +0800

    Update SimpleButton.jsx

--- src/components/simple-button/SimpleButton.jsx
+++ src/components/simple-button/SimpleButton.jsx
@@ -1,8 +1,8 @@
 // @flow
 /**
  * @file [SimpleButton]
- * @author [wangyang65]
- * @email [wangyang65@baidu.com]
+ * @author [mzvast]
+ * @email [mzvast@gmail.com]
  * @create date 2018-07-31 15:26:40
  */
 /* eslint-disable babel/new-cap,operator-linebreak,fecs-export-on-declare */
